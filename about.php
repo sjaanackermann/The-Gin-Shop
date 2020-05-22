22ed1bf0156a8ced466f4f19d619f99fde4b526d
Sjaan Ackermann <sjaanackermann@gmail.com>
2020-05-22 13:35:07 +0200

    about.php - navbar & footer added

--- about.php
+++ about.php
@@ -1,1 +1,146 @@
-<?php ?>
+<?php
+
+//if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
+if(session_id() == '' || !isset($_SESSION)){session_start();}
+
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Contact || The Gin Shop</title>
+
+    <!-- Foundation CSS -->
+    <link rel="stylesheet" href="css/foundation.css" />
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <script src="js/vendor/modernizr.js"></script>
+
+  </head>
+  <body>
+
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="index.php">The Gin Shop</a></h1>
+        </li>
+        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+      </ul>
+
+      <section class="top-bar-section">
+      <!-- Right Nav Section -->
+        <ul class="right">
+          <li><a href="about.php">About</a></li>
+          <li><a href="products.php">Products</a></li>
+          <li><a href="cart.php">View Cart</a></li>
+          <li><a href="orders.php">My Orders</a></li>
+          <li class="active"><a href="contact.php">Contact</a></li>
+          <?php
+
+          if(isset($_SESSION['username'])){
+            echo '<li><a href="account.php">My Account</a></li>';
+            echo '<li><a href="logout.php">Log Out</a></li>';
+          }
+          else{
+            echo '<li><a href="login.php">Log In</a></li>';
+            echo '<li><a href="register.php">Register</a></li>';
+          }
+          ?>
+        </ul>
+      </section>
+    </nav>
+
+    <div class="row" style="margin-top:30px;">
+      <div class="small-12">
+        <p>This is wehre the about details will be includin the team info.</p>
+
+        <p>Why The Gin Shop? </p>
+
+        <footer>
+           <p style="text-align:center; font-size:0.8em;">&copy; The Gin Shop. All Rights Reserved.</p>
+        </footer>
+
+      </div>
+    </div>
+
+
+    <footer>
+        <div class="footer" id="footer">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                        <h3> About </h3>
+                        <ul>
+                            <li> <a href="#"> Our Company </a> </li>
+                            <li> <a href="#"> Our Team </a> </li>
+                            <li> <a href="#"> Terms of Services </a> </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                        <h3> Other </h3>
+                        <ul>
+                            <li> <a href="#"> Contact Us </a> </li>
+                            <li> <a href="#"> Delivery </a> </li>
+                            <li> <a href="faq.php"> FAQ </a> </li>
+                        </ul>
+
+                    </div>
+                    <div class="col-lg-3  col-md-3 col-sm-6 col-xs-12 ">
+                        <h3> Subscribe with Us </h3>
+                        <ul>
+                            <li>
+                                <div class="input-append subscribe-btn text-center">
+                                    <input type="text" class="full text-center" placeholder="Email ">
+                                    <button class="btn  btn-info" type="button"> Subscribe <i class="fa fa-save"> </i> </button>
+                                </div>
+                            </li>
+                        </ul>
+
+                        <ul class="social">
+                            <li> <a href="#"> <i class="fa fa-facebook"> </i> </a> </li>
+                            <li> <a href="#"> <i class="fa fa-twitter"> </i> </a> </li>
+                            <li> <a href="#"> <i class="fa fa-google-plus"> </i> </a> </li>
+                            <li> <a href="#"> <i class="fa fa-instagram"></i> </a> </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+                        <h3> We Accept: </h3>
+                        <ul>
+                            <li class="payments"><i class="fa fa-cc-paypal"></i> PayPal</li>
+                            <li class="payments"><i class="fa fa-cc-mastercard"></i> Master Card</li>
+                            <li class="payments"><i class="fa fa-cc-visa"></i> Visa</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-bottom">
+            <div class="containers">
+                <p> The Gin Shop © 2020. All rights reserved. </p>
+            </div>
+        </div>
+        </div>
+        </div>
+
+    </footer>
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
