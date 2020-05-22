2ab6f7ea114bbff98fad9fa478a2f96b4aff4202
Sjaan Ackermann <sjaanackermann@gmail.com>
2020-05-22 13:23:08 +0200

    config.php updated

--- config.php
+++ config.php
@@ -1,1 +1,8 @@
-<?php ?>
+<?php
+$currency = 'R';
+$db_username = 'root';
+$db_password = 'root';
+$db_name = 'shop';
+$db_host = 'localhost';
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+?>
