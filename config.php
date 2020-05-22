4b9837ec2962e3bcf54b8825fb2cafeb6994670d
Sjaan Ackermann <sjaanackermann@gmail.com>
2020-05-22 15:40:21 +0200

    config.php changed to new database

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $currency = 'R';
 $db_username = 'root';
 $db_password = 'root';
-$db_name = 'shop';
+$db_name = 'gin';
 $db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
