c42c463caf8badd5afee7a5b94741c747c4700e4
Sjaan Ackermann <sjaanackermann@gmail.com>
2020-06-23 11:32:19 +0200

    About page title changed

--- pages/about.php
+++ pages/about.php
@@ -13,7 +13,7 @@ if (session_id() == '' || !isset($_SESSION)) {
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Contact || The Gin Shop</title>
+    <title>About || The Gin Shop</title>
 
     <!-- My CSS -->
     <link rel="stylesheet" href="css/style.css">
