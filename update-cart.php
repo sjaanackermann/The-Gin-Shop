b9ca5c291de591947f6673f1a0f9817109213fbf
Sjaan Ackermann <sjaanackermann@gmail.com>
2020-06-23 11:40:25 +0200

    Update-Cart.php comments added

--- update-cart.php
+++ update-cart.php
@@ -7,24 +7,24 @@ include 'config/config.php';
 $product_id = $_GET['id'];
 $action = $_GET['action'];
 
-
+// Checking if cart is empty
 if($action === 'empty')
   unset($_SESSION['cart']);
 
 $result = $mysqli->query("SELECT qty FROM products WHERE id = ".$product_id);
 
-
+// Switch statement for adding and removing products from the cart
 if($result){
 
   if($obj = $result->fetch_object()) {
 
     switch($action) {
-
+// Add
       case "add":
       if($_SESSION['cart'][$product_id]+1 <= $obj->qty)
         $_SESSION['cart'][$product_id]++;
       break;
-
+// Remove
       case "remove":
       $_SESSION['cart'][$product_id]--;
       if($_SESSION['cart'][$product_id] == 0)
