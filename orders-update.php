a411c6815b0a775ee0dac3d7efca6be70fa2d97e
Sjaan Ackermann <sjaanackermann@gmail.com>
2020-06-23 11:36:56 +0200

    orders-update.php comments added

--- orders-update.php
+++ orders-update.php
@@ -1,35 +1,37 @@
 <?php
 //if (session_status() !== PHP_SESSION_ACTIVE) {session_start();}
-if(session_id() == '' || !isset($_SESSION)){session_start();}
+if (session_id() == '' || !isset($_SESSION)) {
+  session_start();
+}
 
 include 'config/config.php';
 
-if(isset($_SESSION['cart'])) {
+if (isset($_SESSION['cart'])) {
 
   $total = 0;
 
-  foreach($_SESSION['cart'] as $product_id => $quantity) {
+  // Selecting the products from the database with product id
 
-    $result = $mysqli->query("SELECT * FROM products WHERE id = ".$product_id);
+  foreach ($_SESSION['cart'] as $product_id => $quantity) {
 
-    if($result){
+    $result = $mysqli->query("SELECT * FROM products WHERE id = " . $product_id);
 
-      if($obj = $result->fetch_object()) {
+    if ($result) {
 
+      if ($obj = $result->fetch_object()) {
 
+        // Calculating the cost of all procts in cart
         $cost = $obj->price * $quantity;
 
         $user = $_SESSION["username"];
-
+        // Inserting products into orders for logged in user
         $query = $mysqli->query("INSERT INTO orders (product_code, product_name, product_desc, price, units, total, email) VALUES('$obj->product_code', '$obj->product_name', '$obj->product_desc', $obj->price, $quantity, $cost, '$user')");
-
-        if($query){
+        // Updating the quantity of products left after products added to orders
+        if ($query) {
           $newqty = $obj->qty - $quantity;
-          if($mysqli->query("UPDATE products SET qty = ".$newqty." WHERE id = ".$product_id)){
-
+          if ($mysqli->query("UPDATE products SET qty = " . $newqty . " WHERE id = " . $product_id)) {
           }
         }
-
       }
     }
   }
@@ -37,5 +39,3 @@ if(isset($_SESSION['cart'])) {
 
 unset($_SESSION['cart']);
 header("location:success.php");
-
-?>
