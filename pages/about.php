ac6e20fab1a5a7083211b784ab6bbf3955c7d285
Sjaan Ackermann <sjaanackermann@gmail.com>
2020-06-25 11:31:43 +0200

    all Dropdowns working in Navbars

--- pages/about.php
+++ pages/about.php
@@ -15,7 +15,8 @@ if (session_id() == '' || !isset($_SESSION)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>About || The Gin Shop</title>
 
-
+    <!-- My CSS -->
+    <link rel="stylesheet" href="../css/style.css">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">  
@@ -24,8 +25,6 @@ if (session_id() == '' || !isset($_SESSION)) {
     <!-- Foundation CSS -->
     <link rel="stylesheet" href="../css/foundation.css" />
 
-    <!-- My CSS -->
-    <link rel="stylesheet" href="../css/style.css">
 
 
     <script src="js/vendor/modernizr.js"></script>
@@ -208,7 +207,10 @@ if (session_id() == '' || !isset($_SESSION)) {
 
     </footer>
 
-
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="js/vendor/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
     <script>
